02ec7ee41bb9ab60a9ce03fb2c6a05642d4d43c2
kpblc2000 <kpblc2000@gmail.com>
2024-12-20 20:40:31 +0300

    Добавлен проект для nanoCAD, с копрованием кода

---
+++ LoadAssemblies.NCAD/CadCommands/TestCmd.cs
@@ -0,0 +1,22 @@
+﻿using HostMgd.ApplicationServices;
+using LoadAssemblies.NCAD.Views.Windows;
+using Teigha.Runtime;
+
+namespace LoadAssemblies.NCAD.CadCommands
+{
+    public static class TestCmd
+    {
+        [CommandMethod("test-aio")]
+        public static void TestAllInOneCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null)
+            {
+                return;
+            }
+
+            TestWindow win = new TestWindow();
+            Application.ShowModalWindow(win);
+        }
+    }
+}
---
+++ LoadAssemblies.NCAD/Views/Windows/TestWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LoadAssemblies.NCAD.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
