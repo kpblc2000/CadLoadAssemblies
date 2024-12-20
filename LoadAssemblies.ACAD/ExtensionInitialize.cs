e719fd6c6940a2898f8b3da2f6e98e28e7f767f4
kpblc2000 <kpblc2000@gmail.com>
2024-12-20 21:06:50 +0300

    Проверка загрузки dll по запросу в nanoCAD

---
+++ LoadAssemblies.ACAD/ExtensionInitialize.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LoadAssemblies.ACAD
+{
+    public  class ExtensionInitialize : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+        }
+
+        public void Terminate() { }
+
+        private Assembly CurrentDomainOnAssemblyResolve(object Sender, ResolveEventArgs Args)
+        {
+            string name = GetAssemblyName(Args);
+            string path = Path.Combine(Path.GetDirectoryName((typeof(ExtensionInitialize).Assembly.Location)), name + ".dll");
+            if (File.Exists(path))
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+                if (assembly.FullName == Args.Name)
+                {
+                    return assembly;
+                }
+            }
+            return null;
+        }
+        private string GetAssemblyName(ResolveEventArgs args)
+        {
+            if (args.Name.IndexOf(",") > -1)
+            {
+                return args.Name.Substring(0, args.Name.IndexOf(","));
+            }
+            else
+            {
+                return args.Name;
+            }
+        }
+    }
+}
+}
---
+++ LoadAssemblies.NCAD/ExtenxtionInitialize.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Teigha.Runtime;
+
+namespace LoadAssemblies.NCAD
+{
+    public class ExtensionInitialize : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+        }
+
+        public void Terminate() { }
+
+        private Assembly CurrentDomainOnAssemblyResolve(object Sender, ResolveEventArgs Args)
+        {
+            string name = GetAssemblyName(Args);
+            string path = Path.Combine(Path.GetDirectoryName((typeof(ExtensionInitialize).Assembly.Location)), name + ".dll");
+            if (File.Exists(path))
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+                if (assembly.FullName == Args.Name)
+                {
+                    return assembly;
+                }
+            }
+            return null;
+        }
+        private string GetAssemblyName(ResolveEventArgs args)
+        {
+            if (args.Name.IndexOf(",") > -1)
+            {
+                return args.Name.Substring(0, args.Name.IndexOf(","));
+            }
+            else
+            {
+                return args.Name;
+            }
+        }
+    }
+}
