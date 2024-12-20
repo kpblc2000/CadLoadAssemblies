d445ccbab538132f053a08d4513c3d86436033a8
kpblc2000 <kulikaa@pik-industry.ru>
2024-12-20 19:29:10 +0300

    Неработающее окно, привязанное к FontAwesome

---
+++ LoadAssemblies.ACAD/CadCommands/TestCmd.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using LoadAssemblies.ACAD.Views.Windows;
+
+namespace LoadAssemblies.ACAD.CadCommands
+{
+    public static class TestCmd
+    {
+        [CommandMethod("test-aio")]
+        public static void TestAllInOneCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) {
+                return;
+            }
+
+            TestWindow win = new TestWindow();
+            Application.ShowModalWindow(win);
+        }
+    }
+}
---
+++ LoadAssemblies.ACAD/Views/Windows/TestWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LoadAssemblies.ACAD.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
