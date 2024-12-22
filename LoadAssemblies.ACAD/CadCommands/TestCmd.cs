6513c47b0f81becc578618b836ac69434da84614
kpblc2000 <kulikaa@pik-industry.ru>
2024-12-22 19:19:25 +0300

    Пока еще не копируются сторонние dll в выходной каталог

---
+++ LoadAssemblies.ACAD/CadCommands/TestCmd.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using LoadAssemblies.Views.Windows;
+
+namespace LoadAssemblies.ACAD.CadCommands
+{
+    public static class TestCmd
+    {
+        [CommandMethod("test-aio")]
+        public static void TestAllInOneCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null)
+            {
+                return;
+            }
+
+            TestWindow win = new TestWindow();
+            Application.ShowModalWindow(win);
+        }
+    }
+}
--- LoadAssemblies.Views.NET/Windows/TestWindow.xaml.cs
+++ LoadAssemblies.Views.NET/Windows/TestWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace LoadAssemblies.Views.Windows
+namespace LoadAssemblies.Views.NET.Windows
 {
     /// <summary>
     /// Логика взаимодействия для TestWindow.xaml
